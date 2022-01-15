249d21392eff01876374b4ef04e24edf933009ff
Rohit Dixit <RDixit@inspiron15.onmicrosoft.com>
2022-01-15 11:51:01 +0530

    Code changed || Delay Time Increased.

--- Worker.cs
+++ Worker.cs
@@ -22,7 +22,7 @@ namespace dotnetcore_workerservice
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(10000, stoppingToken);
             }
         }
     }
